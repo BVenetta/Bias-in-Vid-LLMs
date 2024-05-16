77f69f403bdc400c849ef83e1be4fa14082f6aed
Sergei <mailbox.sa02@gmail.com>
2024-05-16 19:10:24 +0200

    small changes

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import fiftyone as fo
 import fiftyone.zoo as foz
 
+# Path to missing files
+source_dir = "./videos"
+
 # Download and load 10 samples from the validation split of ActivityNet 200
 dataset = foz.load_zoo_dataset(
     "activitynet-200",
     split="validation",
-    max_samples=10,
+    max_samples=3,
 )
 
 if __name__ == "__main__":
     # Ensures that the App processes are safely launched on Windows
     session = fo.launch_app(dataset)
-    session.dataset = dataset
     session.wait()
